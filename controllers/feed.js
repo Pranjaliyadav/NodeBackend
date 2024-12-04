70cc83c0c3ef7fdf3d573ee9e8393e7001e335f7
Pranjali Yadav <51490690+Pranjaliyadav@users.noreply.github.com>
2024-12-04 21:39:33 +0530

    basics done

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,17 @@
 exports.getPosts = (req, res, next) =>{
-    
+    //will send json response
+    res.status(200).json({
+        posts : [{ title : 'First Post', content : 'nothing!'}],
+       
+    })
+}
+
+exports.createPost = (req, res, next) =>{
+    const title = req.body.title
+    const content = req.body.content
+
+    res.status(201).json({
+        message : 'Post created successfully',
+        post : {id : new Date().toISOString, title, content}
+    })
 }
