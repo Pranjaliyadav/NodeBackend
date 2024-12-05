7f49f473c3afbbb5f2f35bc6c46401c204abfd8d
Pranjali Yadav <51490690+Pranjaliyadav@users.noreply.github.com>
2024-12-05 18:29:04 +0530

    image upload and display done

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 const express = require('express')
 const path = require('path')
-
+const multer = require('multer')
 require('dotenv').config();
 const feedRoutes = require('./routes/feed')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const MONGODB_URI = process.env.MONGO_DB_CONNECTION_STRING
+const { v4: uuidv4} = require('uuid');
+
+
 const app = express()
 
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function(req, file, cb) {
+        cb(null, uuidv4())
+    }
+});
+
+const fileFilter = (req, file, cb) =>{
+    if(file.mimetype === 'image/png' || 
+        file.mimetype === 'image/jpeg' || 
+        file.mimetype === 'image/jpg' 
+    ){
+        cb(null, true)
+
+    }
+    else{
+        cb(null, false)
+    }
+}
 app.use(bodyParser.json()) //parse incoming json data
+app.use(multer({storage, fileFilter}).single('image'))
+
 app.use('/images', express.static(path.join(__dirname, 'images'))) //to parse images statically
 
 
