983b29869220d1acd26f82548919bbc556a10547
Pranjali Yadav <51490690+Pranjaliyadav@users.noreply.github.com>
2024-12-12 15:14:52 +0530

    ugh testing why testing

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,12 +2,46 @@ const expect = require('chai').expect
 
 const authMiddleware = require('../middleware/is-auth')
 
-it('should throw error if token not there', function(){
-    const req = {
-        get : function(headerName){
-            return null
-        }
-    }
+describe('Auth middleware', function() {
 
-    expect(authMiddleware.bind(this, req, {} ,() =>{})).to.throw('Invalid token')
+    it('should throw error if token not there', function(){
+        const req = {
+            get : function(headerName){
+                return null
+            }
+        }
+    
+        expect(authMiddleware.bind(this, req, {} ,() =>{})).to.throw('Invalid token')
+    })
+    
+    it('should throw error if token is only 1 string', function(){
+        const req = {
+            get : function(headerName){
+                return 'abc'
+            }
+        }
+    
+        expect(authMiddleware.bind(this, req, {} ,() =>{})).to.throw()
+    })
+
+    it('should throw error if token is not verified', function(){
+        const req = {
+            get : function(headerName){
+                return 'Bearer abc'
+            }
+        }
+    
+        expect(authMiddleware.bind(this, req, {} ,() =>{})).to.throw()
+    })
+
+    it('should give userId after decoding token', function(){
+        const req = {
+            get : function(headerName){
+                return 'Bearer abc'
+            }
+        }
+        authMiddleware(req, {}, () =>{})
+        expect(req).to.have.property('userId')
+    })
 })
+
--- test/start.js
+++ test/start.js
@@ -1,13 +1,17 @@
 const expect = require('chai').expect
 
-it('should add number correctly', function() { //first arg is just a string describing what your test does 
-    const num1 = 2 
-    const num2 = 3
-    expect(num1+ num2).to.equal(5)
-})
 
-it('should not give a result of 6', function() { //first arg is just a string describing what your test does 
-    const num1 = 2
-    const num2 = 3
-    expect(num1+ num2).not.to.equal()
+describe('random test', function() {
+
+    it('should add number correctly', function() { //first arg is just a string describing what your test does 
+        const num1 = 2 
+        const num2 = 3
+        expect(num1+ num2).to.equal(5)
+    })
+    
+    it('should not give a result of 6', function() { //first arg is just a string describing what your test does 
+        const num1 = 2
+        const num2 = 3
+        expect(num1+ num2).not.to.equal()
+    })
 })
